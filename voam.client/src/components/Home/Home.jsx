442cbb0caff4141b845a7b269591c4019f471c93
Konstantin <kzaraliev@gmail.com>
2024-01-22 21:18:22 +0200

    Add shop now button

--- voam.client/src/components/Home/Home.jsx
+++ voam.client/src/components/Home/Home.jsx
@@ -1,18 +1,26 @@
-import Image from 'react-bootstrap/Image';
+import { Link } from "react-router-dom";
+
+import Image from "react-bootstrap/Image";
+import Path from "../../utils/paths";
 
 import styles from "./Home.module.css";
 import banner from "../../assets/banner-f.png";
-import RecentlyAddedProducts from './RecentlyAddedProducts/RecentlyAddedProducts';
+import RecentlyAddedProducts from "./RecentlyAddedProducts/RecentlyAddedProducts";
 
 function Home() {
-    return (
-        <div className={styles.home}>
-            <h1 className={styles.title}>Voam</h1>
-            <h2 className={styles.slogan}>Elevate Your Urban Vibe with Every Stride!</h2>
-            <Image className={styles.banner} src={banner} fluid />
-            <RecentlyAddedProducts/>
-        </div>
-    );
+  return (
+    <div className={styles.home}>
+      <h1 className={styles.title}>Voam</h1>
+      <h2 className={styles.slogan}>
+        Elevate Your Urban Vibe with Every Stride!
+      </h2>
+      <Image className={styles.banner} src={banner} fluid />
+      <Link to={Path.Items} type="button" className={styles.shopNow}>
+        Shop now
+      </Link>
+      <RecentlyAddedProducts />
+    </div>
+  );
 }
 
 export default Home;
