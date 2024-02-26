cfce605a11ea09f8c2e596d72ee0960bebf00410
Konstantin <kzaraliev@gmail.com>
2024-02-26 22:06:37 +0200

    add responsive css to product details

--- Voam.Server/obj/Debug/net8.0/Voam.Server.AssemblyInfo.cs
+++ Voam.Server/obj/Debug/net8.0/Voam.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Voam.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d5eacdf737bc0aef5d9d5d43613fd8e1165ba0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a66672d33674b16cb392ef845927c673004be5a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Voam.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Voam.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
