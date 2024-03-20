c9f752ca05e21e9cd8102a9065dad6acf74fa67b
Konstantin <kzaraliev@gmail.com>
2024-03-20 19:40:25 +0200

    add carousel to home page

--- voam.client/src/components/Home/RecentlyAddedProducts/RecentlyAddedProduct.jsx
+++ voam.client/src/components/Home/RecentlyAddedProducts/RecentlyAddedProduct.jsx
@@ -1,23 +1,29 @@
 import Card from "react-bootstrap/Card";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
+import Carousel from "react-bootstrap/Carousel";
 
 import styles from "./RecentlyAddedProducts.module.css";
-import Path from "../../../utils/paths"
+import Path from "../../../utils/paths";
 
 import { Link } from "react-router-dom";
 
 export default function RecentlyAddedProduct({ id, name, price, image }) {
+  const imgSrc = `data:image/jpeg;base64,${image}`;
 
-    const imgSrc = `data:image/jpeg;base64,${image}`;
-
-    return (
-        <Card className={styles.card}>
-            <Card.Img variant="top" src={imgSrc} className={styles.img} />
-            <Card.Body className={styles.cardBody}>
-                <Card.Title className={styles.cardTitle}>{name}</Card.Title>
-                <Card.Subtitle className={["mb-2", styles.cardPrice]}>Price: {price} lv.</Card.Subtitle>
-                <Button as={Link} to={`${Path.Items}/${id}`} variant="success">View Details</Button>
-            </Card.Body>
-        </Card>
-    );
+  return (
+    <div className={styles.carouselItemContent}>
+      <Card className={styles.card}>
+        <Card.Img variant="top" src={imgSrc} className={styles.img} />
+        <Card.Body className={styles.cardBody}>
+          <Card.Title className={styles.cardTitle}>{name}</Card.Title>
+          <Card.Subtitle className={["mb-2", styles.cardPrice]}>
+            Price: {price} lv.
+          </Card.Subtitle>
+          <Button as={Link} to={`${Path.Items}/${id}`} variant="success">
+            View Details
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
--- voam.client/src/components/Home/RecentlyAddedProducts/RecentlyAddedProducts.jsx
+++ voam.client/src/components/Home/RecentlyAddedProducts/RecentlyAddedProducts.jsx
@@ -1,26 +1,73 @@
 import { useEffect, useState } from "react";
 import styles from "./RecentlyAddedProducts.module.css";
 import RecentlyAddedProduct from "./RecentlyAddedProduct";
-import * as productService from "../../../services/productService"
+import * as productService from "../../../services/productService";
+import Carousel from "react-bootstrap/Carousel";
 
 function RecentlyAddedProducts() {
-    const [products, setProducts] = useState();
+  const [products, setProducts] = useState();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-    useEffect(() => {
-        productService.getLatest().then(res => setProducts(res))
-    }, []);
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width to state
+      setWindowWidth(window.innerWidth);
+    }
 
-    return (
-        <div className={styles.recentProducts}>
-            <h1 className={styles.title}>Recently added products:</h1>
+    // Add event listener
+    window.addEventListener("resize", handleResize);
 
-            <div className={styles.containerProducts}>
-                {products === undefined ? <p>Loading...</p> : products.map((product) => (
-                    <RecentlyAddedProduct key={product.id} id={product.id} name={product.name} price={product.price} image={product.image.imageData} />
-                ))}
-            </div>
-        </div>
-    );
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    productService.getLatest().then((res) => setProducts(res));
+  }, []);
+
+  return (
+    <div className={styles.recentProducts}>
+      <h1 className={styles.title}>Recently added products:</h1>
+
+      <div className={styles.containerProducts}>
+        {windowWidth < 768 ? (
+          <Carousel className={styles.carousel} data-bs-theme="dark" indicators={false} >
+            {products === undefined ? (
+              <p>Loading...</p>
+            ) : (
+              products.map((product, index) => (
+                <Carousel.Item key={index}>
+                  <RecentlyAddedProduct
+                    key={product.id}
+                    id={product.id}
+                    name={product.name}
+                    price={product.price}
+                    image={product.image.imageData}
+                  />
+                </Carousel.Item>
+              ))
+            )}
+          </Carousel>
+        ) : products === undefined ? (
+          <p>Loading...</p>
+        ) : (
+          products.map((product) => (
+            <RecentlyAddedProduct
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              price={product.price}
+              image={product.image.imageData}
+            />
+          ))
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default RecentlyAddedProducts;
