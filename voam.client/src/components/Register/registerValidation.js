1ea37257f112327f203ddd6b040ec83a32f98dbf
Konstantin <kzaraliev@gmail.com>
2024-04-13 16:13:10 +0300

    Add regex for names in register

--- voam.client/src/components/Register/registerValidation.js
+++ voam.client/src/components/Register/registerValidation.js
@@ -8,6 +8,8 @@ const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}/;
 // 10 digits, can include spaces, dashes, parentheses.
 const phoneNumberRules = /^\(?([0-9]{3})\)?[- ]?([0-9]{3})[- ]?([0-9]{4})$/;
 
+const namesRules = /^[A-Za-z]+$/;
+
 const registerValidation = Yup.object({
     [RegisterFormKeys.Email]: Yup.string()
         .min(6, 'Email must be 6 characters or more')
@@ -15,10 +17,14 @@ const registerValidation = Yup.object({
         .email('Invalid email'),
     [RegisterFormKeys.FirstName]: Yup.string()
         .min(2, 'First name must be 2 characters or more')
-        .required('Enter first name'),
+        .trim('First name must not include leading or trailing spaces')
+        .required('Enter first name')
+        .matches(namesRules, "Only letters allowed. No numbers or special characters."),
     [RegisterFormKeys.LastName]: Yup.string()
         .min(3, 'Last name must be 3 characters or more')
-        .required('Enter last name'),
+        .trim('First name must not include leading or trailing spaces')
+        .required('Enter last name')
+        .matches(namesRules, "Only letters allowed. No numbers or special characters."),
     [RegisterFormKeys.Password]: Yup.string()
         .min(5, 'Password must be 5 characters or more')
         .required('Enter your password')
